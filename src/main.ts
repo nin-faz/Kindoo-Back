df309d5df7df2fc20c80b3dc053552b2547bc6a4
Jordy-6 <jper220803@gmail.com>
2025-06-13 19:22:04 +0200

    test3

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupArena } from './bullArena/arena';
+import { startWorker } from './bullMQ/queue.consumer';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,5 +17,7 @@ async function bootstrap() {
   setupArena(expressApp);
 
   await app.listen(process.env.PORT ?? 3000);
+
+  startWorker();
 }
 bootstrap();
